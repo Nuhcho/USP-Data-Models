5728643a2f045edf882842fadf242f6efce50196
Nuhcho <130648216+Nuhcho@users.noreply.github.com>
2023-06-20 13:01:52 -0700

    Add files via upload

--- USP-Data-Models-Python/Maindata.py
+++ USP-Data-Models-Python/Maindata.py
@@ -1,4 +1,3 @@
-import codecs
 import csv
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -21,17 +20,19 @@ url = r"https://usp-data-models.broadband-forum.org/tr-181-2-15-1-usp.html#devic
 # Open browser
 browser.get(url)
 
-# Find all elements with class name "object"
-objects = browser.find_elements(By.CLASS_NAME, "object")
-# Find all elements with class name "mountpoint-object"
-mountpoint_object = browser.find_elements(By.CLASS_NAME, "command")
+# Find all elements by CSS SELECTOR in order to add them to the list in the proper order.
+objects = browser.find_elements(By.CSS_SELECTOR, ".object, .command")
+
+
+
 # Declaration for all the lists
 objectList = []
-mountpoint_objectList = []
 
-# The first object is unnecessary so here it is removed
-objects = objects[1:]
-mountpoint_object = mountpoint_object[1:]
+
+# The first two objects is unnecessary so here it is removed
+objects = objects[2:]
+
+
 
 #Method to remove the \xad at the end of each text
 def remove_non_breaking_hyphen(text):
@@ -39,7 +40,6 @@ def remove_non_breaking_hyphen(text):
 
 #For breaking out of nested for loops
 breakObjLoop = False
-breakMobjLoop = False
 
 for obj in objects:
     # Find the nested elements with tag name "td" that contain the desired values
@@ -47,34 +47,18 @@ for obj in objects:
     # Temp list to store the parameter values
     tempObjectList = []
     #Contains a statement that checks for a the length of the parameter which causes the code to break out of all loops. This to prevent selenium from grabbing data beyond the table.
-    for parameter in parameters:
-        if len(parameters) == 1:
+    for index, parameter in enumerate(parameters):
+        if len(parameters) == 1 or (index == 1 and remove_non_breaking_hyphen(parameter.text) == '-'):
             breakObjLoop = True
             break
         tempObjectList.append(remove_non_breaking_hyphen(parameter.text))
     
+    #Breaks out of nested for loop
     if breakObjLoop:
         break
     
     objectList.append(tempObjectList)
 
-for mobj in mountpoint_object:
-    # Find the nested elements with tag name "td" that contain the desired values
-    parameters = mobj.find_elements(By.TAG_NAME, "td")
-    # Temp list to store the parameter values
-    tempMountPointObjectList = []
-    
-    #Contains a statement that checks for a - in the 2nd spot which causes the code to break out of all loops. This to prevent selenium from grabbing data beyond the table.
-    for index, parameter in enumerate(parameters):
-        if index == 1 and remove_non_breaking_hyphen(parameter.text) == '-':
-            breakMobjLoop = True
-            break
-        tempMountPointObjectList.append(remove_non_breaking_hyphen(parameter.text))
-    
-    if breakMobjLoop:
-        break
-    
-    mountpoint_objectList.append(tempMountPointObjectList)
 
 #Encoding changes the encoding type from cp1252 to utf-8 thus fixing the error for encoding special characters like apostrophes and commas
 with open('information.csv', "w", newline='', encoding = "utf-8") as information:
@@ -82,4 +66,3 @@ with open('information.csv', "w", newline='', encoding = "utf-8") as information
     writer.writerow(['Name', 'Type', 'Write', 'Description', 'Object Default', 'Version'])
     #Sorts through the lists with these big lists and writes them in rows
     writer.writerows(objectList)
-    writer.writerows(mountpoint_objectList)
